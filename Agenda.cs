23afe517514e99fb6ec3629088f7e4bd95c8d476
unknown <rafacs1404@gmail.com>
2020-07-03 10:33:17 -0300

    CRUD (exceto Update) criado

--- Agenda.cs
+++ Agenda.cs
@@ -15,5 +15,48 @@ namespace WhatsAppConsole
             return $"\nNome:{c.Nome};Telefone:{c.Telefone}";
         }
 
+        public void Cadastrar(Contatos c){
+            string[] linha = new string[] {PrepararLinha(c)};
+            File.AppendAllLines(PATH, linha);
+        }
+
+        public void Excluir(string _termo){
+            List<string> linhas = new List<string>();
+            using(StreamReader file = new StreamReader(PATH)){
+                string linha;
+                while((linha = file.ReadLine()) != null){
+                    linhas.Add(linha);
+                }
+                linhas.RemoveAll(z => z.Contains(_termo));
+            }
+
+            using(StreamWriter output = new StreamWriter(PATH)){
+                output.Write(String.Join(Environment.NewLine, linhas.ToArray()));
+            }
+        } 
+
+        public void Ler(){
+            List<Contatos> cont = new List<Contatos>();
+
+            string[] linhas = File.ReadAllLines(PATH);
+            foreach (string linha in linhas)
+            {
+                string[] dado = linha.Split(";"); 
+            
+                Contatos c = new Contatos();
+                c.Nome = Separar(dado[0]);
+                c.Telefone = Separar(dado[1]);
+
+                cont.Add(c);
+                System.Console.WriteLine($"{c}");
+            }         
+        }
+
+        public string Separar(string dado){
+            return dado.Split(':')[1];
+        }
+
+
+
     }
 }
--- iAgenda.cs
+++ iAgenda.cs
@@ -1,8 +1,15 @@
+using System.IO;
+
 namespace WhatsAppConsole
 {
     public interface iAgenda
     {
-    
+        void Cadastrar(Contatos c);
+        void Excluir(string _termo);
+        void Ler();
+
+
+
     
     }
 }
