20bc1831d5712ae1d9d06371ab303fa39faefd12
unknown <rafacs1404@gmail.com>
2020-07-03 09:38:40 -0300

    Primeiro Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WhatsAppConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
